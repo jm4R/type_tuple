b24214bfa1dee94b40121f984375d67c0ef8fd99
jm4R <mar.jaskolka@gmail.com>
2018-04-21 14:30:10 +0200

    Add test cases for size

--- test/test.cpp
+++ test/test.cpp
@@ -81,6 +81,70 @@ struct test_fixture {
       assert(500 == db.get<cache_size>());
     }
   }
+
+  void test_size() {
+    enum some_enum { v1, v2 };
+    enum class some_enum_class { v1, v2 };
+
+    using t01 = mj::explicit_type<char, class some_tag>;
+    static_assert(sizeof(char) == sizeof(t01),
+                  "size of underlying type should equal");
+    using t02 = mj::explicit_type<int, class some_tag>;
+    static_assert(sizeof(int) == sizeof(t02),
+                  "size of underlying type should equal");
+    using t03 = mj::explicit_type<long, class some_tag>;
+    static_assert(sizeof(long) == sizeof(t03),
+                  "size of underlying type should equal");
+    using t04 = mj::explicit_type<unsigned, class some_tag>;
+    static_assert(sizeof(unsigned) == sizeof(t04),
+                  "size of underlying type should equal");
+    using t05 = mj::explicit_type<float, class some_tag>;
+    static_assert(sizeof(float) == sizeof(t05),
+                  "size of underlying type should equal");
+    using t06 = mj::explicit_type<double, class some_tag>;
+    static_assert(sizeof(double) == sizeof(t06),
+                  "size of underlying type should equal");
+    using t07 = mj::explicit_type<bool, class some_tag>;
+    static_assert(sizeof(bool) == sizeof(t07),
+                  "size of underlying type should equal");
+    using t08 = mj::explicit_type<int *, class some_tag>;
+    static_assert(sizeof(int *) == sizeof(t08),
+                  "size of underlying type should equal");
+    using t09 = mj::explicit_type<std::string, class some_tag>;
+    static_assert(sizeof(std::string) == sizeof(t09),
+                  "size of underlying type should equal");
+    using t10 = mj::explicit_type<std::shared_ptr<int>, class some_tag>;
+    static_assert(sizeof(std::shared_ptr<int>) == sizeof(t10),
+                  "size of underlying type should equal");
+    using t11 = mj::explicit_type<some_enum, class some_tag>;
+    static_assert(sizeof(some_enum) == sizeof(t11),
+                  "size of underlying type should equal");
+    using t12 = mj::explicit_type<some_enum_class, class some_tag>;
+    static_assert(sizeof(some_enum_class) == sizeof(t12),
+                  "size of underlying type should equal");
+
+    struct plain_data {
+      char v01;
+      int v02;
+      long v03;
+      unsigned v04;
+      float v05;
+      double v06;
+      bool v07;
+      int *v08;
+      std::string v09;
+      std::shared_ptr<int> v10;
+      some_enum v11;
+      some_enum_class v12;
+    };
+
+    using tuple_data = mj::explicit_tuple<t01, t02, t03, t04, t05, t06, t07,
+                                          t08, t09, t10, t11, t12>;
+
+    static_assert(
+        sizeof(plain_data) == sizeof(tuple_data),
+        "size of tuple should be the same as size of plain data struct");
+  }
 };
 
 int main() {
@@ -93,7 +157,7 @@ int main() {
   test.test_explicit_type<float>(5.5);
   test.test_explicit_type<double>(5);
   test.test_explicit_type<bool>(true);
-  test.test_explicit_type<bool>(true);
+  test.test_explicit_type<bool>(false);
   int a;
   test.test_explicit_type<int *>(&a);
   test.test_explicit_type<std::string>("test");
@@ -105,6 +169,7 @@ int main() {
   test.test_explicit_type<some_enum_class>(some_enum_class::v2);
   //---
   test.test_explicit_tuple();
+  test.test_size();
 
   std::cout << "All tests passed" << std::endl;
 }
